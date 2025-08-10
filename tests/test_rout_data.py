16bc1ecf1b05a4eebbeeea4af99b5e7ed94f8b77
Youssef <134784751+youssefhusain@users.noreply.github.com>
2025-08-10 17:05:08 +0300

    Update test_rout_data.py

--- tests/test_rout_data.py
+++ tests/test_rout_data.py
@@ -13,6 +13,7 @@ fake_controllers = types.ModuleType("controllers")
 class FakeDataController:
     def validate_uploaded_file(self, file):
         return True, "FILE_UPLOAD_SUCCESS"
+
     def generate_unique_filepath(self, orig_file_name, project_id):
         return f"/tmp/{orig_file_name}", "fake_file_id"
 
@@ -20,11 +21,22 @@ class FakeProjectController:
     def get_project_path(self, project_id):
         return "/tmp"
 
+class FakeProcessController:
+    def __init__(self, project_id):
+        self.project_id = project_id
+
+    def get_file_content(self, file_id):
+        return "some file content"
+
+    def process_file_content(self, file_content, file_id, chunk_size, overlap_size):
+        return [{"chunk": "part1"}, {"chunk": "part2"}]
+
 fake_controllers.DataController = FakeDataController
 fake_controllers.ProjectController = FakeProjectController
+fake_controllers.ProcessController = FakeProcessController
+
 sys.modules["controllers"] = fake_controllers
 # ===== End Mock =====
-
 
 from routes.data import data_router
 from helpers.config import Settings, get_settings
@@ -70,7 +82,6 @@ def test_upload_data_invalid_file(tmp_path):
     test_file = tmp_path / "bad.txt"
     test_file.write_bytes(b"bad content")
 
-    
     with open(test_file, "rb") as f:
         response = client.post(
             "/api/v1/data/upload/123",
@@ -80,3 +91,30 @@ def test_upload_data_invalid_file(tmp_path):
     assert response.status_code == 400
     data = response.json()
     assert data["signal"] == "INVALID_FILE"
+
+def test_process_endpoint_success():
+    payload = {
+        "file_id": "fake_file_id",
+        "chunk_size": 100,
+        "overlap_size": 10
+    }
+    response = client.post("/api/v1/data/process/123", json=payload)
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) == 2
+    assert data[0]["chunk"] == "part1"
+
+def test_process_endpoint_failed():
+
+    fake_controllers.ProcessController.process_file_content = lambda *args, **kwargs: None
+
+    payload = {
+        "file_id": "fake_file_id",
+        "chunk_size": 100,
+        "overlap_size": 10
+    }
+    response = client.post("/api/v1/data/process/123", json=payload)
+    assert response.status_code == 400
+    data = response.json()
+    assert data["signal"] == "PROCESSING_FAILED"
